b7d28f29f507fb486b4d8b88ceef05417e7063d2
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-09-08 17:13:40 +0200

    Add sample directory

--- infrastructure/create-azure-ai-foundry-resources.sh
+++ infrastructure/create-azure-ai-foundry-resources.sh
@@ -2,13 +2,20 @@
 
 # Execute this script to deploy the needed Azure AI Foundry resources to execute the application.
 # For this, you need Azure CLI installed: https://learn.microsoft.com/cli/azure/install-azure-cli
+# If already installed, check the version with `az --version` and make sure it is up to date with `az upgrade`
 
 echo "Setting up environment variables..."
 echo "----------------------------------"
 PROJECT="hack2025agenticnews"
 RESOURCE_GROUP="rg-$PROJECT"
-LOCATION="swedencentral"
+LOCATION="swedencentral" # check https://learn.microsoft.com/azure/ai-foundry/reference/region-support
 TAG="$PROJECT"
+UNIQUE_IDENTIFIER=${GITHUB_USER:-$(whoami)}
+
+
+echo "Logging in..."
+echo "----------------------------------"
+az login
 
 echo "Creating the resource group..."
 echo "------------------------------"
