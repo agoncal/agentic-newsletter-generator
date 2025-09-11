6d84d2a29096a0f2e5bdd00f2e67c4a2334585db
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-09-11 15:55:49 +0200

    Fixing temperature

--- samples/src/main/java/ai/agentic/newslettergen/sample/InvokeGitHubMCPServer.java
+++ samples/src/main/java/ai/agentic/newslettergen/sample/InvokeGitHubMCPServer.java
@@ -43,7 +43,7 @@ public class InvokeGitHubMCPServer {
             .apiKey(AZURE_AI_FOUNDRY_KEY)
             .baseUrl(AZURE_AI_FOUNDRY_ENDPOINT)
             .modelName(AGENT_RELEASE_MODEL)
-            .temperature(0.3)
+            .temperature(1.0)
             .logRequests(IS_LOGGING_ENABLED)
             .logResponses(IS_LOGGING_ENABLED)
             .build();
